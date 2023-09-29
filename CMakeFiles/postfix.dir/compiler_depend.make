91a19d780061a1ee8db150dee12ee17a3038eb51
Viktor Guylherme <68589396+ViktorG13@users.noreply.github.com>
2023-09-29 17:33:13 +0000

    :tada: Add .gitignore

--- CMakeFiles/postfix.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for postfix.
-# This may be replaced when dependencies are built.
--- Debug/CMakeFiles/postfix.dir/src/parser.cpp.o.d
+++ Debug/CMakeFiles/postfix.dir/src/parser.cpp.o.d
@@ -1,3 +1,2 @@
 CMakeFiles/postfix.dir/src/parser.cpp.o: \
- /home/kaleb/Documentos/Cursos/Compiladores/src/parser.cpp \
- /usr/include/stdc-predef.h
+ /workspaces/Compiladores/src/parser.cpp /usr/include/stdc-predef.h
